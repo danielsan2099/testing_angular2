c3b054debc57595bed0fe61f4c71f69549e756e9
Daniel santarriaga <Daniel santarriaga>
2016-11-15 17:22:56 -0700

    Continuamos con el notify-me

--- src/app/notify-me/notify-me.component.ts
+++ src/app/notify-me/notify-me.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notify-me.component.css']
 })
 export class NotifyMeComponent implements OnInit {
-
+  is_visible = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public btn_click(){
+    //Averiguar como colocar la ventana en los margenes visibles de la pantalla
+    //var elemento = document.getElementById('id_del_elemento');
+    //var posicion = elemento.getBoundingClientRect();
+    this.is_visible = !this.is_visible;
   }
 
 }
